766e43d48957138b3266daa87a162bf8a07c21c7
Satyam <rsatyam866@gmail.com>
2021-12-26 10:41:15 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeappAndroid()
+	{
+		System.out.println ("NativeappAndroid"); //NativeappAndroid-> method.
+		
+	}
+
+	@Test
+	public void iosApps()
+	{
+		System.out.println ("iosApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,23 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	//testng xml file from maven
+	//how to execute all test cases from test folder -mvn test
+	
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println ("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println ("elementsUI");
+	}
+
+
+}
